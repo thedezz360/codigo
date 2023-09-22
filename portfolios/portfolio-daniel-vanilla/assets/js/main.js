1a4cd9947b016760aa69c7980858582fd1c52f6b
daniel <thedezz360@gmail.com>
2023-09-22 13:11:29 +0200

    hero

--- portfolios/portfolio-daniel-vanilla/assets/js/main.js
+++ portfolios/portfolio-daniel-vanilla/assets/js/main.js
@@ -1,10 +1,26 @@
-/** ==========>>>>  Mostrar/Ocultar menu  <<<<========== */
+/** ==========>>>>  Mostrar menu  <<<<========== */
 const navMenu = document.getElementById('nav-menu')
-
-function showHideMenu(){
+console.log(navMenu)
+function showMenu(){
+  console.log({navMenu})
   if(navMenu){
-    navMenu.classList.toggle('show-menu')
+    navMenu.classList.contains('show-menu') ?
+    navMenu.classList.remove('show-menu') :
+    navMenu.classList.add('show-menu')
   }
 }
 
-document.getElementById('menu-responsive').addEventListener('click', showHideMenu)
+document.getElementById('menu-responsive').addEventListener('click', showMenu)
+
+/** ==========>>>>  Ocultar menu  <<<<========== */
+
+
+function hideMenu (){
+  if(navMenu){
+    navMenu.classList.contains('show-menu') ?
+    navMenu.classList.remove('show-menu') :
+    navMenu.classList.add('show-menu')
+  }
+}
+
+document.getElementById('navClose').addEventListener('click', hideMenu)
